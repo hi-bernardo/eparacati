9753980f6c69a4865bb57650109a5856b3e1c0cd
luiz-developer <luizandreleal100@gmail.com>
2023-09-08 13:18:54 -0300

    estilo dos links

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 //Components
 import Menu from "./components/layout/Menu";
 import Footer from "./components/layout/Footer";
+import Anchor from "./components/layout/Anchor";
 
 function App() {
    return (
       <>
          <Menu />
          <Footer />
+         <Anchor title="Isso é um link" href="https://github.com" target="_blank"/>
       </>
    );
 }
