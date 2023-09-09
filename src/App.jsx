7fedce3644fa64a1999ea8c3ade6c1e448db26fd
luiz-developer <luizandreleal100@gmail.com>
2023-09-09 08:27:04 -0300

    component anchor excluido

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 //Components
 import Menu from "./components/layout/Menu";
 import Footer from "./components/layout/Footer";
-import Anchor from "./components/layout/Anchor";
 
 function App() {
    return (
       <>
          <Menu />
          <Footer />
-         <Anchor
-            title="Isso é um link"
-            href="https://github.com"
-            target="_blank"
-         />
       </>
    );
 }
