f7065cf6835bd72f23ddcb5fcfed6ac42a389ad5
Brazoo <hiagobernard031@gmail.com>
2023-09-14 08:49:30 -0300

    navbar nas telas maiores alteradas

--- src/components/layout/Menu/index.jsx
+++ src/components/layout/Menu/index.jsx
@@ -29,21 +29,21 @@ function Menu() {
 
    return (
       <>
-         <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-            <div class="px-3 py-3 lg:px-5 lg:pl-3">
-               <div class="flex items-center justify-between">
-                  <div class="flex items-center justify-start">
+         <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-800">
+            <div className="px-3 py-3 lg:px-5 lg:pl-3">
+               <div className="flex items-center justify-between">
+                  <div className="flex items-center justify-start">
                      <button
                         onClick={toggleMenu}
                         data-drawer-target="logo-sidebar"
                         data-drawer-toggle="logo-sidebar"
                         aria-controls="logo-sidebar"
                         type="button"
-                        class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
+                        className="md:hidden inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
                      >
-                        <span class="sr-only">Open sidebar</span>
+                        <span className="sr-only">Abrir menu</span>
                         <svg
-                           class="w-6 h-6"
+                           className="w-6 h-6"
                            aria-hidden="true"
                            fill="currentColor"
                            viewBox="0 0 20 20"
@@ -56,55 +56,74 @@ function Menu() {
                            ></path>
                         </svg>
                      </button>
-                     <a href="/" class="flex ml-2 md:mr-24">
+                     <a href="/" className="flex ml-2 md:mr-24">
                         <img
                            src={Logo}
-                           class="h-10 mr-3"
+                           className="h-10 mr-3"
                            alt="EP Aracati Logo"
                         />
                      </a>
+                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                           <a href="#" className="block py-2 pl-3 pr-4 text-white bg-primary rounded md:bg-transparent md:text-primary md:p-0 dark:text-white md:dark:text-primary" aria-current="page">Início</a>
+                        </li>
+                        <li>
+                           <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                        </li>
+                        <li>
+                           <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Livros</a>
+                        </li>
+                        <li>
+                           <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                        </li>
+                        <li>
+                           <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        </li>
+                        </ul>
+                     </div>
                   </div>
-                  <div class="flex items-center">
-                     <div class="flex items-center ml-3">
+                  <div className="flex items-center">
+                     <div className="flex items-center ml-3">
                         <div>
                            <button
                               onClick={toggleProfileDropDown}
                               type="button"
-                              class="flex text-sm bg-gray-900 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                              className="flex text-sm bg-gray-900 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                               aria-expanded="false"
                               data-dropdown-toggle="dropdown-user"
                            >
-                              <span class="sr-only">Open user menu</span>
+                              <span className="sr-only">Open user menu</span>
                               <img
-                                 class="w-8 h-8 rounded-full"
+                                 className="w-8 h-8 rounded-full"
                                  src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                                  alt="user photo"
                               />
                            </button>
                         </div>
                         <div
-                           class="z-50 top-7 right-3 absolute hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-800 dark:divide-gray-600"
+                           className="z-50 top-7 right-3 absolute hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-800 dark:divide-gray-600"
                            id="dropdown-user"
                         >
-                           <div class="px-4 py-3" role="none">
+                           <div className="px-4 py-3" role="none">
                               <p
-                                 class="text-sm text-gray-900 dark:text-white"
+                                 className="text-sm text-gray-900 dark:text-white"
                                  role="none"
                               >
                                  Aluno
                               </p>
                               <p
-                                 class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                                 className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
                                  role="none"
                               >
                                  aluno@aluno.ce.gov.br
                               </p>
                            </div>
-                           <ul class="py-1" role="none">
+                           <ul className="py-1" role="none">
                               <li>
                                  <a
                                     href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                                     role="menuitem"
                                  >
                                     Perfil
@@ -113,7 +132,7 @@ function Menu() {
                               <li>
                                  <a
                                     href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                                     role="menuitem"
                                  >
                                     Configurações
@@ -122,7 +141,7 @@ function Menu() {
                               <li>
                                  <a
                                     href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                                     role="menuitem"
                                  >
                                     Sair
@@ -138,28 +157,28 @@ function Menu() {
 
          <aside
             id="logo-sidebar"
-            class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 dark:bg-gray-900 dark:border-gray-700"
+            className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 dark:bg-gray-900 dark:border-gray-700"
             aria-label="Sidebar"
          >
-            <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-900">
-               <ul class="space-y-2 font-medium">
+            <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-900">
+               <ul className="space-y-2 font-medium">
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <HiOutlineHome className="text-2xl" />
-                        <span class="ml-3">Início</span>
+                        <span className="ml-3">Início</span>
                      </a>
                   </li>
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <HiOutlineNewspaper className="text-2xl" />
-                        <span class="flex-1 ml-3 whitespace-nowrap">Blog</span>
-                        <span class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-gray-300">
+                        <span className="flex-1 ml-3 whitespace-nowrap">Blog</span>
+                        <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-gray-300">
                            3
                         </span>
                      </a>
@@ -167,10 +186,10 @@ function Menu() {
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <HiOutlineBookOpen className="text-2xl" />
-                        <span class="flex-1 ml-3 whitespace-nowrap">
+                        <span className="flex-1 ml-3 whitespace-nowrap">
                            Livros
                         </span>
                      </a>
@@ -178,10 +197,10 @@ function Menu() {
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <svg
-                           class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                           className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                            aria-hidden="true"
                            xmlns="http://www.w3.org/2000/svg"
                            fill="currentColor"
@@ -189,16 +208,16 @@ function Menu() {
                         >
                            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                         </svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
+                        <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
                      </a>
                   </li>
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <svg
-                           class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                           className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                            aria-hidden="true"
                            xmlns="http://www.w3.org/2000/svg"
                            fill="currentColor"
@@ -206,7 +225,7 @@ function Menu() {
                         >
                            <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
                         </svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap">
+                        <span className="flex-1 ml-3 whitespace-nowrap">
                            Products
                         </span>
                      </a>
@@ -214,10 +233,10 @@ function Menu() {
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <svg
-                           class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                           className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                            aria-hidden="true"
                            xmlns="http://www.w3.org/2000/svg"
                            fill="none"
@@ -231,7 +250,7 @@ function Menu() {
                               d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
                            />
                         </svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap">
+                        <span className="flex-1 ml-3 whitespace-nowrap">
                            Sign In
                         </span>
                      </a>
@@ -239,10 +258,10 @@ function Menu() {
                   <li>
                      <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 group"
                      >
                         <svg
-                           class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                           className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                            aria-hidden="true"
                            xmlns="http://www.w3.org/2000/svg"
                            fill="currentColor"
@@ -252,7 +271,7 @@ function Menu() {
                            <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
                            <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
                         </svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap">
+                        <span className="flex-1 ml-3 whitespace-nowrap">
                            Sign Up
                         </span>
                      </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}"
    ],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            primary: "#4ec027"
+         }
+      },
    },
    plugins: [],
 }
-
