cd374321284ba9796fe6c17a1f3acaf6a61296cf
luiz-developer <luizandreleal100@gmail.com>
2023-09-23 17:35:52 -0300

    🐛 fix: erro na funcao

--- src/components/layout/Select/index.jsx
+++ src/components/layout/Select/index.jsx
@@ -9,7 +9,6 @@ function Select({ id, label, placeholder, options }) {
          </label>
          <select
             id={id}
-            defaultValue=""
             className="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-gray-50 dark:focus:ring-primary dark:focus:border-primary"
          >
             <option value="" disabled selected>
