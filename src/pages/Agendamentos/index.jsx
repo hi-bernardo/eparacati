868331b75792e9ae8bdec1d04bc3ad88b43cc3f4
Mateus Ferreira <mateusjunior803@gmail.com>
2023-09-23 20:19:56 -0300

    empty states adicionado

--- src/pages/Agendamentos/index.jsx
+++ src/pages/Agendamentos/index.jsx
@@ -1,69 +1,16 @@
 import Table from "../../components/context/Table";
-import Button from "../../components/context/Button";
+import EmptyStates from "../../components/context/EmptyStates";
 import Alert from "../../components/layout/Alert";
 
 function Agendamentos() {
-   return (
-      <main className="mt-20 mx-auto w-full max-w-screen-xl p-4 flex flex-col gap-8">
-         <h1 className="text-gray-900 text-3xl font-bold dark:text-white">
-            Agendamentos
-         </h1>
-         <div className="flex flex-col gap-4">
-            <h2 className="text-gray-900 text-2xl font-semibold dark:text-white">
-               Projetores
-            </h2>
-            <Table
-               value=""
-               date=""
-               startTime=""
-               endTime=""
-               teacher=""
-               classroom=""
-            />
-         </div>
-         <div className="flex flex-col gap-4">
-            <h2 className="text-gray-900 text-2xl font-semibold dark:text-white">
-               Salas
-            </h2>
-            <Table
-               identification="Nome"
-               value=""
-               date=""
-               startTime=""
-               endTime=""
-               teacher=""
-               classroom=""
-            />
-         </div>
-         <div className="flex flex-col gap-4">
-            <h2 className="text-gray-900 text-2xl font-semibold dark:text-white">
-               Notebooks
-            </h2>
-            <Table
-               value=""
-               date=""
-               startTime=""
-               endTime=""
-               teacher=""
-               classroom=""
-            />
-         </div>
-         <div className="flex flex-col gap-4">
-            <h2 className="text-gray-900 text-2xl font-semibold dark:text-white">
-               Sons
-            </h2>
-            <Table
-               value=""
-               date=""
-               startTime=""
-               endTime=""
-               teacher=""
-               classroom=""
-            />
-         </div>
-         <Button />
-      </main>
-   );
+	return (
+		<main className="mt-20 mx-auto w-full max-w-screen-xl p-4 flex flex-col gap-8">
+			<h1 className="text-gray-900 text-3xl font-bold dark:text-white">
+				Agendamentos
+			</h1>
+			<EmptyStates/>
+		</main>
+	);
 }
 
 export default Agendamentos;
