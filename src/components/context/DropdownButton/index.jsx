e54dc2dce27bd5fa2de706e8e71eb94faa776cd9
luiz-developer <luizandreleal100@gmail.com>
2023-09-23 21:52:25 -0300

    cores da borda do empty state mudadas

--- src/components/context/DropdownButton/index.jsx
+++ src/components/context/DropdownButton/index.jsx
@@ -1,60 +1,61 @@
 import { HiPlus } from "react-icons/hi2";
 
-function DropdownButton() {
-	const toggleDropdown = () => {
-		document.getElementById("dropdown").classList.toggle("hidden");
-	};
+function DropdownButton({dropdownFunc}) {
 
-	return (
-		<div>
-			{/* Button */}
-			<button
-				className="flex gap-2 justify-center items-center bg-primaryDark hover:bg-primary text-white focus:ring-2 focus:ring-primaryLight text-sm font-semibold rounded px-3 py-1"
-				onClick={toggleDropdown}
-			>
-				<HiPlus className=" text-white text-xl" />
-				Agendar
-			</button>
+   const toggleDropdown = () => {
+      document.getElementById("dropdown").classList.toggle("hidden");
+   };
+   
+   return (
+      <div>
+         {/* Button */}
+         <button
+            className="flex gap-2 justify-center items-center bg-primaryDark hover:bg-primary text-white text-sm font-semibold rounded-lg px-3 py-2"
+            onClick={toggleDropdown}
+         >
+            <HiPlus className="text-white text-xl" />
+            Agendar
+         </button>
 
-			{/* Dropdown */}
-			<div
-				className="bottom-70 absolute my-4 text-base drop-shadow bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 dark:border-2 dark:border-gray-500 w-30"
-				id="dropdown"
-			>
-				<a
-					href="#"
-					className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-					role="menuitem"
-				>
-					Projetores
-				</a>
+         {/* Dropdown */}
+         <div
+            className="hidden bottom-70 absolute my-4 text-base drop-shadow bg-gray-50 divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 border dark:border-gray-500 w-30"
+            id="dropdown"
+         >
+            <a
+               href="#"
+               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+               role="menuitem"
+            >
+               Projetores
+            </a>
 
-				<a
-					href="#"
-					className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-					role="menuitem"
-				>
-					Notebooks
-				</a>
+            <a
+               href="#"
+               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+               role="menuitem"
+            >
+               Notebooks
+            </a>
 
-				<a
-					href="#"
-					className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-					role="menuitem"
-				>
-					Caixas de Som
-				</a>
+            <a
+               href="#"
+               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+               role="menuitem"
+            >
+               Caixas de Som
+            </a>
 
-				<a
-					href="#"
-					className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-					role="menuitem"
-				>
-					Salas
-				</a>
-			</div>
-		</div>
-	);
+            <a
+               href="#"
+               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+               role="menuitem"
+            >
+               Salas
+            </a>
+         </div>
+      </div>
+   );
 }
 
 export default DropdownButton;
--- src/components/context/EmptyStates/index.jsx
+++ src/components/context/EmptyStates/index.jsx
@@ -5,21 +5,24 @@ import DropdownButton from "../DropdownButton";
 import { HiOutlineFolderPlus } from "react-icons/hi2";
 
 function EmptyStates() {
+   const toggleDropdown = () => {
+      document.getElementById("dropdown").classList.toggle("hidden");
+   };
 
-	return (
-		<div className="px-4 py-12 bg-white dark:bg-gray-800 w-full rounded-lg flex flex-col gap-3 items-center text-sm border-2 border-zinc-300 dark:border-slate-400 shadow-md">
-			<HiOutlineFolderPlus className="text-zinc-400 dark:text-white text-4xl" />
-			<div className="text-center">
-				<h2 className="text-black dark:text-white font-semibold">
-					Sem agendamentos
-				</h2>
-				<p className="mt-1 text-zinc-500 dark:text-slate-200">
-					Comece criando um novo agendamento.
-				</p>
-			</div>
-			<DropdownButton/>
-		</div>
-	);
+   return (
+      <div className="px-4 py-12 bg-gray-50 dark:bg-gray-800 w-full rounded-lg flex flex-col gap-3 items-center text-sm border border-gray-200 dark:border-gray-700 shadow-md">
+         <HiOutlineFolderPlus className="text-zinc-400 dark:text-white text-4xl" />
+         <div className="text-center">
+            <h2 className="text-black dark:text-white font-semibold">
+               Sem agendamentos
+            </h2>
+            <p className="mt-1 text-zinc-500 dark:text-gray-100">
+               Comece criando um novo agendamento.
+            </p>
+         </div>
+         <DropdownButton />
+      </div>
+   );
 }
 
 export default EmptyStates;
